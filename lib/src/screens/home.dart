69049bb9e6618d7d7b1fc1ed603391e8c1b908bb
Samuel <samuelketema01@gmail.com>
2024-04-11 22:23:01 +0300

    The body of app bar is alterd in order to show some change whenever the screen gets touched

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../widgets/cat.dart';
 
 class Home extends StatefulWidget{
@@ -8,18 +9,58 @@ class Home extends StatefulWidget{
 
 }
 
-class HomeState extends State<Home>{
+class HomeState extends State<Home> with TickerProviderStateMixin{
+ late Animation<double> catAnimation;
+ late AnimationController catController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds:2),
+      vsync: this);
+    catAnimation = Tween(begin: 0.0 , end: 100.0).animate(
+      CurvedAnimation(parent: catController, curve: Curves.easeIn),
+      
+      );
+      catController.forward();
+  }
+  onTap() {
+    if (catController.status == AnimationStatus.completed){
+      catController.reverse();
+    }
+    else if (catController.status ==  AnimationStatus.dismissed){
+        catController.forward();
+    }
+  }
 Widget build(context){
   return Scaffold(
     appBar: AppBar(
       title: Text('Animation'),
       ),
-    body:buildAnimation(),
+    body:GestureDetector(
+      child: buildAnimation(),
+      onTap: onTap,
+
+    ),
   );
 }
 
 Widget buildAnimation(){
-  return Cat();
+  return AnimatedBuilder(
+    animation: catAnimation, 
+    builder: (context , child){
+      return Container(
+        child: child,
+        margin: EdgeInsets.only(top: catAnimation.value),
+      );
+    },
+    child: Cat()
+    );
+    
 }
+
 
 }
