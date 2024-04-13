15ee262e18809bcecc09b2148364e22fed842f15
Samuel <samuelketema01@gmail.com>
2024-04-13 14:13:08 +0300

    lets try to animate the arms

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -12,11 +12,22 @@ class Home extends StatefulWidget{
 class HomeState extends State<Home> with TickerProviderStateMixin{
  late Animation<double> catAnimation;
  late AnimationController catController;
+ late Animation<double> boxAnimation;
+ late AnimationController boxController;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    
+    boxController = AnimationController( 
+      duration: Duration(seconds:1),
+      vsync:this
+    );
+
+    boxAnimation = Tween(begin:0.5 , end: -11.0).animate(
+      CurvedAnimation(parent: boxController, curve: Curves.linear)
+    );
 
     catController = AnimationController(
       duration: Duration(milliseconds:10),
@@ -48,7 +59,8 @@ Widget build(context){
         children: [
           buildAnimation(),
           buildBox(),
-          leftArm()
+          leftArm(),
+          RightArm()
           
         ],
       ),
@@ -86,17 +98,41 @@ Widget buildBox() {
 
 Widget leftArm(){
   return Positioned(
-    top: 3,
+    top:3,
     left: 5,
-    child: Transform.rotate(
-    angle:-1.39*pi,
-    alignment: Alignment.topLeft,
+    child:AnimatedBuilder(
+    
+    animation: boxAnimation,
     child: Container(
       height: 10,
       width: 125,
       color: Colors.red,
     ),
-    )
+
+   builder: (context,child){
+    return Transform.rotate(
+    angle:boxAnimation.value,
+    alignment: Alignment.topLeft,
+    
+    );
+   }
+   ),
+   );
+  
+}
+Widget RightArm(){
+  return Positioned(
+    top: 4,
+    left: 65,
+    child:  Transform.rotate(
+    angle:pi*5.5/4 ,
+    alignment: Alignment.topRight,
+    child:  Container(
+    height: 10,
+    width: 125,
+    color: Colors.red,
+  )
+  )
   );
 }
 }
